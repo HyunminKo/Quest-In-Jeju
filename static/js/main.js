7cb041efd5ebdf8ec2d4c280b74ca21239679d4e
HyunminKo <hyunmin.ko4578@gmail.com>
2019-08-02 19:58:25 +0900

    Add index,mypage

---
+++ static/js/main.js
@@ -0,0 +1,48 @@
+let menuFlag = false;
+let state = "quest.png";
+
+$(document).ready(function() {
+  $(".navbar-brand").hide();
+  showInitIcon(state);
+});
+function showInitIcon(state) {
+  $("#img_1").attr("src", "static/img/" + state);
+  $("#link_1").show();
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function showStateIcons(t) {
+  if (!menuFlag) {
+    var styleElem = document.head.appendChild(document.createElement("style"));
+    styleElem.innerHTML = "#menu:after {border: 0;}";
+
+    $(".menu").addClass("opened");
+    $("#header-title").hide();
+
+    let nameOfImages = ["quest.png", "home.png", "gamer.png", "timeline.png"];
+    let index = nameOfImages.indexOf(state);
+    if (index !== -1) nameOfImages.splice(index, 1);
+
+    for (let i = 2; i <= 4; i++) {
+      $("#img_" + i).attr("src", "static/img/" + nameOfImages[i - 2]);
+    }
+    await sleep(100);
+    for (let i = 2; i <= 4; i++) {
+      $("#link_" + i).show();
+    }
+
+    menuFlag = true;
+  } else {
+    document.head.removeChild(document.head.lastChild);
+    $(".menu").removeClass("opened");
+    $(".navbar-brand").hide();
+
+    let img = $("#img_" + t.id.split("_")[1]);
+    state = img.attr("src").split("/")[2];
+    showInitIcon(state);
+
+    $("#header-title").show();
+    menuFlag = false;
+  }
+}
