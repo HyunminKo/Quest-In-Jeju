fb78220f479a1ae772f64cf861a76edb8f52d09f
chunbong <cdy0266@gmail.com>
2019-08-04 00:04:56 +0900

    [생성]

---
+++ src/user/UserDAO.java
@@ -0,0 +1,32 @@
+package user;
+
+import jdbcUtil.JdbcTemplate;
+import rowmapper.UserRowMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    JdbcTemplate jdbc = new JdbcTemplate();
+    public void insert(){
+        String name = "testtest";
+        String password = "testtest";
+        String email = "testtest@testtest.org";
+        String sql = "insert into user (email,name,password) values(?,?,?)";
+        try {
+            jdbc.update(sql,name,password,email);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public List<UserVO> findAll() {
+        List<UserVO> ls = null;
+        String sql = "select * from user";
+        try {
+            ls = jdbc.query(sql,new UserRowMapper());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ls;
+    }
+}
