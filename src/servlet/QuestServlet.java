b739be62a8f1e160d7ef48e021c130662fe54a7e
chunbong <cdy0266@gmail.com>
2019-08-06 19:01:11 +0900

    Create QuestServlet

---
+++ src/servlet/QuestServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import quest.QuestDAO;
+import quest.QuestVO;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+
+
+public class QuestServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        QuestDAO dao = new QuestDAO();
+        List<QuestVO> ls = dao.findAll();
+
+
+    }
+
+}
