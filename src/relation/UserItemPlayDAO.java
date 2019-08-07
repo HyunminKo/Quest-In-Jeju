aba5939b09621b9eb7fa88bd690041a0d10843a8
chunbong <cdy0266@gmail.com>
2019-08-07 14:24:24 +0900

    아이템 수행 상태 바꾸기 기능

--- src/relation/UserItemPlayDAO.java
+++ src/relation/UserItemPlayDAO.java
@@ -1,22 +1,39 @@
-//package relation;
-//
-//import jdbcUtil.JdbcTemplate;
-//import rowmapper.UserItemPlayRowMapper;
-//
-//public class UserItemPlayDAO {
-//
-//
-//    JdbcTemplate jdbc = new JdbcTemplate();
-//
-//    public UserQuestPlayVO queryForObject(user_id, item_id){
-//        String sql = "select user_id, item_id from user_item_play where user_id=? and item_id=?";
-//        UserItemPlayVO vo = new UserItemPlayVO();
-//        UserItemPlayRowMapper rowMapper = new UserItemPlayRowMapper();
-//        try {
-//            vo = jdbc.queryForObject(sql, rowMapper, user_id, item_id);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return vo;
-//    }
-//}
+package relation;
+
+import jdbcUtil.JdbcTemplate;
+import rowmapper.UserItemPlayRowMapper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class UserItemPlayDAO {
+
+
+    JdbcTemplate jdbc = new JdbcTemplate();
+
+    public UserItemPlayVO queryForObject(Long user_id, Long item_id){
+        String sql = "select user_id, item_id from user_item_play where user_id=? and item_id=?";
+        UserItemPlayVO vo = new UserItemPlayVO();
+        UserItemPlayRowMapper rowMapper = new UserItemPlayRowMapper();
+        try {
+            vo = jdbc.queryForObject(sql, rowMapper, user_id, item_id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vo;
+    }
+
+    public int update(Long user_id, Long item_id) {
+        int rc = 0;
+        String sql = "update user_quest_play set is_completed = ? where user_id = ? and item_id = ?";
+        try {
+            rc = jdbc.update(sql, 1, user_id, item_id);
+            if (rc != 0) {
+                System.out.println("성공");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rc;
+    }
+}
--- src/servlet/UserItemPlayServlet.java
+++ src/servlet/UserItemPlayServlet.java
@@ -2,11 +2,9 @@ package servlet;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import quest.QuestItemVO;
+import relation.UserItemPlayDAO;
 import relation.UserItemPlayVO;
-import relation.UserQuestPlayVO;
-import rowmapper.QuestItemRowMapper;
-import rowmapper.UserItemPlayRowMapper;
+
 
 
 import javax.servlet.http.HttpServlet;
@@ -14,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.List;
 
 
 public class UserItemPlayServlet extends HttpServlet {
@@ -36,11 +35,23 @@ public class UserItemPlayServlet extends HttpServlet {
             String method = (String) jsonObject.get("method");
 
             // 한 줄 검색
-            if("queryForObject".equals(method)) {
+//            if("queryForObject".equals(method)) {
+//                Long user_id = (Long)jsonObject.get("user_id");
+//                Long item_id = (Long)jsonObject.get("item_id");
+//
+//                UserItemPlayDAO dao = new UserItemPlayDAO();
+//                UserItemPlayVO vo = new UserItemPlayVO();
+//
+//                vo = dao.queryForObject(user_id, item_id);
+//            }
+
+            if("update".equals(method)){
                 Long user_id = (Long)jsonObject.get("user_id");
                 Long item_id = (Long)jsonObject.get("item_id");
-            }
 
+                UserItemPlayDAO dao = new UserItemPlayDAO();
+                dao.update(user_id, item_id);
+            }
 
 
 
